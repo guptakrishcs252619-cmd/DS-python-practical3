babfcc83672e77ab321b145d7cb009d4214b1788
guptakrishcs252619-cmd <guptakrishcs252619@gmail.com>
2026-08-27 12:20:46 +0530

    Add files via upload

---
+++ python for data sci p8.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tips = sns.load_dataset("tips")
+
+print(tips.head())
+
+sns.scatterplot(x="total_bill", y="tip", data=tips)
+
+plt.title(" krish 085 Restaurant Bill vs Tip")
+plt.xlabel("Total Bill")
+plt.ylabel("Tip")
+
+plt.show()
+print("krish 085")
